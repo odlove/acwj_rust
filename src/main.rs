490f457aa84d86fa3500e7c2df1a321fd37bfd43
v9pzm7 <751000925@qq.com>
2025-05-29 23:54:15 +0800

    01-1 搭好骨架, 读取文件

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(value_name = "INPUT")]
+    name: String,
+}
+
+fn main() -> std::io::Result<()> {
+    let args = Cli::parse();
+    // println!("{:?}", args);
+
+    let content = std::fs::read_to_string(args.name)?;
+
+    for lines in content.lines() {
+        println!("{lines}");
+    }
+
+    Ok(())
 }
